de8c11e5b394aa8362f2f7db4f66ced0232953be
mp3lol <85530830+mp3lol@users.noreply.github.com>
2021-06-07 23:21:44 -0500

    Add files via upload

--- Batch Authentication .bat
+++ Batch Authentication .bat
@@ -9,29 +9,35 @@
 
 @echo off
 
-::powershell commands
+
+ping www.google.com -n 1 -w 1000 >nul
+if errorlevel 1 (echo An error has occured. Please connect to internet and try again. & timeout /t 3 >nul & exit /b) else (goto connected)
+
+
+:connected
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-powershell -Command "Invoke-WebRequest [PASTEBIN] -OutFile C:\Windows\System32\auth1.txt"
+powershell -Command "$progressPreference = 'silentlyContinue'; Invoke-WebRequest [PASTEBIN] -OutFile C:\Windows\System32\auth.txt"
 
-::setting variables
+
+if not exist "C:\Windows\System32\auth.txt" echo An error has occured. Please disable antivirus and try again. & timeout /t 3 >nul & exit /b
 for /f "tokens=2 delims==" %%A in ('wmic csproduct get uuid /format:value ^| find "="') do set uuid=%%A
-setlocal enabledelayedexpansion
-set content=
-for /F "delims=" %%i in (C:\Windows\System32\auth1.txt) do set content=!content! %%i
+if not exist "C:\Windows\System32\auth.txt" echo An error has occured. Please try again. & timeout /t 3 >nul & exit /b
 
-::auth
-find "%uuid%" C:\Windows\System32\auth1.txt >nul
-if "%errorlevel%"=="0" (
-  echo Authenticated.
-) else (
-  echo %uuid% | CLIP
-  echo Not Authenticated.
-  echo HWID copied to clipboard.
-)
+find "%uuid%" C:\Windows\System32\auth.txt >nul
+if "%errorlevel%"=="0" (goto success) else (goto fail)
 
-::extra
-del /f /q C:\Windows\System32\auth1.txt
+
+:fail
+del /f /q C:\Windows\System32\auth.txt
+echo %uuid% | CLIP
+echo Not Authenticated.
+echo HWID copied to clipboard.
+pause >nul
+
+
+:success
+del /f /q C:\Windows\System32\auth.txt
+echo Authenticated.
 pause >nul
 
 ::---------------------------------------------------------------------------------------------------------
-
